d910a14e2976bed386bc524e174b3a556249687f
justtfelix <justttfelix@gmail.com>
2023-03-05 10:35:02 +0400

    Added theme colors

---
+++ src/interfaces/theme.ts
@@ -0,0 +1,14 @@
+export interface Theme {
+  colors: {
+    primary: {
+      peach: string;
+      black: string;
+      white: string;
+    };
+    secondary: {
+      lightPeach: string;
+      lightGrey: string;
+      darkGrey: string;
+    };
+  };
+}
---
+++ src/theme/defaultTheme.ts
@@ -0,0 +1,14 @@
+export const defaultTheme = {
+  colors: {
+    primary: {
+      peach: "#E7816B",
+      black: "#1D1C1E",
+      white: "#FFFFFF",
+    },
+    secondary: {
+      lightPeach: "#FFAD9B",
+      lightGrey: "#F1F3F5",
+      darkGrey: "#333136",
+    }
+  },
+}
