d62c774e6276d16c37aa1af2cbb2ee597842b1e8
justtfelix <justttfelix@gmail.com>
2023-03-07 19:21:45 +0400

    Added pages

---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as Home } from "./Home";
+export { default as Design } from "./Design";
+export { default as About } from "./About";
+export { default as Locations } from "./Locations";
+export { default as Contact } from "./Contact";
