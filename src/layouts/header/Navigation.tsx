6f44144c075b9454e0639c6cdc88cda65b51d0d6
justtfelix <justttfelix@gmail.com>
2023-03-12 23:33:18 +0400

    Generated navigation links using map

--- src/layouts/header/Navigation.tsx
+++ src/layouts/header/Navigation.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { NavContent } from "../../components";
+import { navLinks } from "../../data";
 
 function Navigation() {
   return (
     <NavContent>
       <ul>
-        <li>
-          <Link to="about">our company</Link>
-        </li>
-        <li>
-          <Link to="locations">locations</Link>
-        </li>
-        <li>
-          <Link to="contact">contact</Link>
-        </li>
+        {navLinks.map((item) => {
+          const { id, title, to } = item;
+
+          return (
+            <li key={id}>
+              <Link to={to}>{title}</Link>
+            </li>
+          )
+        })}
       </ul>
     </NavContent>
   )
