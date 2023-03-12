d3a02ca4c135561b5b2c7edaf60758bea24f3d57
justtfelix <justttfelix@gmail.com>
2023-03-12 23:29:41 +0400

    Added navigation links data

---
+++ src/data/navLinks.ts
@@ -0,0 +1,17 @@
+export const navLinks = [
+  {
+    id: 1,
+    title: "our company",
+    to: "about",
+  },
+  {
+    id: 2,
+    title: "locations",
+    to: "locations",
+  },
+  {
+    id: 3,
+    title: "contact",
+    to: "contact",
+  },
+]
